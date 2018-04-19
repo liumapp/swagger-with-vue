353d68b92ecab0525e40dd4c439fb08a41ce26f6
liumapp <liumapp.com@gmail.com>
2018-04-19 11:03:25 +0800

    config swagger-server

--- swagger-server/src/main/java/com/liumapp/demo/swagger/controller/IndexController.java
+++ swagger-server/src/main/java/com/liumapp/demo/swagger/controller/IndexController.java
@@ -1,30 +1,22 @@
 package com.liumapp.demo.swagger.controller;
 
-import com.liumapp.demo.swagger.entity.Guest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by liumapp on 9/28/17.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@Controller
+@RestController
 @RequestMapping("/")
 public class IndexController {
 
-    @Autowired
-    private Guest guest;
-
     @GetMapping("/")
     public String index (ModelMap model) {
-
-        model.addAttribute("name" , guest.getName());
-        model.addAttribute("sex" , guest.getSex());
-        return "index";
+        return "hello , this is Index Controller";
     }
 
 }
